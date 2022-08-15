27a4ce9f04fd33075f508f632a03daf1005e507b
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2022-08-15 10:04:49 +0700

    カテゴリーページ作成。見出し作成。

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -5,12 +5,10 @@ interface Props {
 
 export default function Heading({ title, subTitle }: Props) {
   return (
-    <div className="text-center space-y-3">
-      <h1 className="font-bold text-4xl lg:text-5xl leading-none">{title}</h1>
-      {subTitle ? (
-        <p className="lg:text-xl leading-none lg:leading-none">{subTitle}</p>
-      ) : null}
-      <div className="bg-secondary dark:bg-soft-white h-1 w-20 mx-auto"></div>
+    <div className="text-center space-y-1 lg:space-y-3">
+      <h1 className="font-bold text-4xl lg:text-5xl">{title}</h1>
+      {subTitle ? <p className="lg:text-xl">{subTitle}</p> : null}
+      <div className="bg-secondary dark:bg-soft-white h-0.5 w-20 mx-auto"></div>
     </div>
   );
 }
